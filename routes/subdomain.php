e7383c9f2a2d702002676463b5a3784b314efdfc
SashoCv <sasocvetanoski@gmail.com>
2024-02-03 20:14:39 +0100

    Add test route on subdomain

--- routes/subdomain.php
+++ routes/subdomain.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::domain('dev.nomad-cloud.in')->group(function () {
     dd('Reached subdomain route');
     Route::get('searchCompany', [SearchController::class, 'searchCompany']);
+    Route::get('test', function () {
+        return 'This is a test route on the subdomain';
+    });
 });
 
      
