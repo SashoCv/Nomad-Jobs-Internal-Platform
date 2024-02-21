b3c624cabf790907f1e76e7ede8f68c729d2116a
Saso Cvetanoski <sasocvetanoski@gmail.com>
2024-02-21 22:23:40 +0100

    Add created_at, updated_at, and deleted_at fields to job postings query

--- app/Http/Controllers/CompanyJobController.php
+++ app/Http/Controllers/CompanyJobController.php
@@ -31,7 +31,7 @@ class CompanyJobController extends Controller
 
             $allJobPostings = DB::table('company_jobs')
                 ->join('companies', 'company_jobs.company_id', '=', 'companies.id')
-                ->select('company_jobs.id', 'company_jobs.company_id', 'company_jobs.job_title', 'company_jobs.number_of_positions', 'company_jobs.job_description', 'companies.nameOfCompany')
+                ->select('company_jobs.id', 'company_jobs.company_id', 'company_jobs.job_title', 'company_jobs.number_of_positions', 'company_jobs.job_description', 'companies.nameOfCompany', 'company_jobs.created_at', 'company_jobs.updated_at', 'company_jobs.deleted_at')
                 ->get();
 
 
@@ -45,7 +45,7 @@ class CompanyJobController extends Controller
                 $allJobPostings = DB::table('company_jobs')
                     ->join('companies', 'company_jobs.company_id', '=', 'companies.id')
                     ->where('company_jobs.company_id', Auth::user()->company_id)
-                    ->select('company_jobs.id', 'company_jobs.company_id', 'company_jobs.job_title', 'company_jobs.number_of_positions', 'company_jobs.job_description', 'companies.nameOfCompany')
+                    ->select('company_jobs.id', 'company_jobs.company_id', 'company_jobs.job_title', 'company_jobs.number_of_positions', 'company_jobs.job_description', 'companies.nameOfCompany', 'company_jobs.created_at', 'company_jobs.updated_at', 'company_jobs.deleted_at')
                     ->get();
 
                 return response()->json([
