f7c1e5ddebef859546053d98fef93451e224ed4e
Saso Cvetanoski <sasocvetanoski@gmail.com>
2024-04-07 19:16:01 +0200

    Refactor CompanyJobController index method to filter job postings based on assigned jobs

--- app/Http/Controllers/CompanyJobController.php
+++ app/Http/Controllers/CompanyJobController.php
@@ -83,13 +83,19 @@ class CompanyJobController extends Controller
             ], 200);
         } else if (Auth::user()->role_id == 4) {
 
+            $assignedJobs = AssignedJob::where('user_id', Auth::user()->id)->get();
+
+            $companyJobIds = [];
+            foreach ($assignedJobs as $assignedJob) {
+                $companyJobIds[] = $assignedJob->company_job_id;
+            }
+
             $allJobPostings = DB::table('company_jobs')
                 ->join('companies', 'company_jobs.company_id', '=', 'companies.id')
-                ->where('company_jobs.user_id', Auth::user()->company_id)
+                ->whereIn('company_jobs.id', $companyJobIds)
                 ->select('company_jobs.id', 'company_jobs.company_id', 'company_jobs.job_title', 'company_jobs.number_of_positions', 'company_jobs.job_description', 'companies.nameOfCompany', 'company_jobs.created_at', 'company_jobs.updated_at', 'company_jobs.deleted_at')
                 ->where('company_jobs.deleted_at', null)
                 ->get();
-
 
             return response()->json([
                 "status" => "success",
