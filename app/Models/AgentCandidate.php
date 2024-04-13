55177e09c57320a4c508498b74374474792c6990
Saso Cvetanoski <sasocvetanoski@gmail.com>
2024-04-13 21:54:56 +0200

    Update AgentCandidate model with new fillable properties

--- app/Models/AgentCandidate.php
+++ app/Models/AgentCandidate.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class AgentCandidate extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'company_job_id',
+        'candidate_id',
+    ];
 }
