8807cd4f8035d7e49af21272a6ed4dff83444933
Saso Cvetanoski <sasocvetanoski@gmail.com>
2024-04-21 17:37:44 +0200

    Add migration to update table candidates and add addedBy column

---
+++ database/migrations/2024_04_21_153427_update_table_candidates_add_added_by_column.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->foreignId('addedBy')->nullable()->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('candidates', function (Blueprint $table) {
+            $table->dropForeign(['addedBy']);
+            $table->dropColumn('addedBy');
+        });
+    }
+};
