e66ef30a76f7e4192d58ad87a74c2bcafe0bf15e
SashoCv <sasocvetanoski@gmail.com>
2024-09-28 12:04:32 +0200

    change title in unpaid invoices

--- resources/views/unpaidInvoicesExcel.blade.php
+++ resources/views/unpaidInvoicesExcel.blade.php
@@ -1,8 +1,11 @@
-@component('mail::message')
-    # Unpaid Invoices Report
-
-    Attached is the Excel file containing all unpaid invoices.
-
-    Thanks,<br>
-    Nomad Team
-@endcomponent
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Unpaid Invoices</title>
+</head>
+<body>
+<h1>Unpaid Invoices Report</h1>
+<p>Please find attached the Excel file containing the unpaid invoices.</p>
+</body>
+</html>
