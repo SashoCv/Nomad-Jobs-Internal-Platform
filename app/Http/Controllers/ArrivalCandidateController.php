b56fff9680038af92bf15b1faed26a7746c767d0
SashoCv <sasocvetanoski@gmail.com>
2025-01-27 08:46:40 +0100

    Refactor date filtering logic in ArrivalCandidateController

--- app/Http/Controllers/ArrivalCandidateController.php
+++ app/Http/Controllers/ArrivalCandidateController.php
@@ -39,6 +39,12 @@ class ArrivalCandidateController extends Controller
                 $fromDate = Carbon::createFromFormat('m-d-Y', $fromDate)->format('d-m-Y');
                 $toDate = Carbon::createFromFormat('m-d-Y', $toDate)->format('d-m-Y');
                 $query->whereBetween('status_date', [$fromDate, $toDate]);
+            } elseif ($fromDate) {
+                $fromDate = Carbon::createFromFormat('m-d-Y', $fromDate)->format('d-m-Y');
+                $query->where('status_date', '>=', $fromDate);
+            } elseif ($toDate) {
+                $toDate = Carbon::createFromFormat('m-d-Y', $toDate)->format('d-m-Y');
+                $query->where('status_date', '<=', $toDate);
             }
 
             $query->orderByRaw("STR_TO_DATE(status_date, '%d-%m-%Y') ASC");
