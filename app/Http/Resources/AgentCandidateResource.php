8b17514ba2ce9a0d0f76877f2ece03e7ad23657c
SashoCv <sasocvetanoski@gmail.com>
2025-02-25 23:26:56 +0100

    Refactor AgentCandidateResource to format created_at date in toArray method

--- app/Http/Resources/AgentCandidateResource.php
+++ app/Http/Resources/AgentCandidateResource.php
@@ -16,7 +16,7 @@ class AgentCandidateResource extends JsonResource
             'id' => $this->id,
             'user_id' => $this->user_id,
             'status_for_candidate_from_agent_id' => $this->status_for_candidate_from_agent_id,
-            'created_at' => $this->created_at->format('Y-m-d'),
+            'created_at' => $this->created_at->format('d-m-Y'),
             'company' => $this->companyJob && $this->companyJob->company ? [
                 'id' => $this->companyJob->company->id,
                 'name' => $this->companyJob->company->nameOfCompany,
