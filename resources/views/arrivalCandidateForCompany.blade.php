2256acfebdd03174739da59819fb5e1517de02f1
SashoCv <sasocvetanoski@gmail.com>
2025-03-05 11:14:53 +0100

    refactor: Optimize image loading in arrivalCandidateForCompany view

--- resources/views/arrivalCandidateForCompany.blade.php
+++ resources/views/arrivalCandidateForCompany.blade.php
@@ -51,7 +51,10 @@
     <p>Здравейте,</p>
     <p>Следният кандидат ще пристигне скоро:</p>
     @if($data['personPicture'])
-    <img src="{{ `https://www.nomad-cloud.in/storage/` . $data['personPicture'] }}" alt="Nomad Partners" style="display: block; margin: 0 auto; width: 200px; height: auto;">
+        <img src="data:image/png;base64,{{ base64_encode(file_get_contents(storage_path('app/public/' . $data['personPicture']))) }}" alt="1">
+        <img src="https://www.nomad-cloud.in/storage/{{ $data['personPicture'] }}?cache-bust={{ time() }}" alt="2">
+        <img src="https://www.nomad-cloud.in/storage/{{ urlencode($data['personPicture']) }}" alt="3">
+
     @endif
     <div class="info">
         <p><strong>Име на кандидат:</strong> {{ $data['candidateName'] }}</p>
