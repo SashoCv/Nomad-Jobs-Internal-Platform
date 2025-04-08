0f57ae7b545684e01a7bd2220c506ea82d57bbcc
SashoCv <sasocvetanoski@gmail.com>
2025-04-08 16:23:32 +0200

    refactor: Update notification message for agent adding candidate for assigned job

--- app/Http/Controllers/AgentCandidateController.php
+++ app/Http/Controllers/AgentCandidateController.php
@@ -159,8 +159,8 @@ class AgentCandidateController extends Controller
             }
 
             $notificationData = [
-                'message' => 'Agent' . ' ' . Auth::user()->name . ' ' .  'added candidate for company job' . ' ' . $getCompanyJob->job_title,
-                'type' => 'Agent' . ' ' . Auth::user()->name . ' ' .  'added candidate for company job' . ' ' . $getCompanyJob->job_title,
+                'message' => 'Agent' . ' ' . Auth::user()->firstName . ' ' .  'added candidate for company job' . ' ' . $getCompanyJob->job_title,
+                'type' => 'Agent' . ' ' . Auth::user()->firstName . ' ' .  'added candidate for company job' . ' ' . $getCompanyJob->job_title,
             ];
 
             $notification = NotificationRepository::createNotification($notificationData);
