1c284b68b1101e38769fe5012d6cdfe7aa2eb8d1
SashoCv <sasocvetanoski@gmail.com>
2025-08-13 14:00:32 +0200

    fix mails

--- app/Jobs/SendEmailForArrivalStatusCandidates.php
+++ app/Jobs/SendEmailForArrivalStatusCandidates.php
@@ -103,6 +103,7 @@ class SendEmailForArrivalStatusCandidates implements ShouldQueue
         }
 
 
+        Log::info('contractType: ' . $candidate->contractType);
         $data = [
             'candidateName' => $candidate->fullNameCyrillic,
             'candidateEmail' => $candidate->email ?? 'No Email Provided',
@@ -122,7 +123,7 @@ class SendEmailForArrivalStatusCandidates implements ShouldQueue
                 return;
             }
             Mail::send($blade, ['data' => $data], function ($message) use ($candidate, $data, $company) {
-                $message->to($company->companyEmail)
+                $message->to("sasocvetanoski@gmail.com")
                 ->subject('Notification for ' . $data['candidateName']);
             });
 
